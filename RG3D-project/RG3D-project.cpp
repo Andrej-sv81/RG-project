6711a07c6322b781591204a5e1a9f684f8840136
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2025-04-04 15:32:24 +0200

    ModelLoading

--- RG3D-project/RG3D-project.cpp
+++ RG3D-project/RG3D-project.cpp
@@ -1,32 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <chrono>
-#include <thread>
 #include <map>
 #include <string>
 #include <stdlib.h>
-#include <iomanip> 
+#include <iomanip>
+#include <thread>
+#include <chrono>
 
-#include <GL/glew.h>
+#include <GL/glew.h>   
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include <ft2build.h>
-#include FT_FREETYPE_H  
+#include FT_FREETYPE_H
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "headers/stb_image.h"
+
 #include "headers/camera.h"
+#include "headers/shader.h"
+#include "headers/model.h"
 
 using namespace std;
 
+unsigned int compileShader(GLenum type, const char* source);
+unsigned int createShader(const char* vsSource, const char* fsSource);
 static unsigned loadImageToTexture(const char* filePath);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+void capFPS();
+GLuint loadCubemap(std::vector<std::string> faces);
+
+struct Planet {
+    Model model;
+    glm::vec3 position;
+    glm::vec3 rotationAxis;
+    float rotationSpeed;
+    glm::vec3 scale;
+    float radius;
+};
 
 struct Character {
     unsigned int TextureID;  // ID handle of the glyph texture
@@ -36,14 +58,29 @@ struct Character {
 };
 void renderText(unsigned int shader, string text, float x, float y, float scale, glm::vec3 color, map<char, Character>& Characters, unsigned int VAO, unsigned int VBO);
 
+// settings
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
 const double targetFPS = 60.0;
 const double frameDuration = 1.0 / targetFPS;
 static auto lastFrameTime = chrono::high_resolution_clock::now();
-void capFPS();
 
 
-int main()
+int main(void)
 {
+
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++ INICIJALIZACIJA ++++++++++++++++++++++++++++++++++++++++++++++++++++++
     if (!glfwInit())
     {
         cout << "GLFW Biblioteka se nije ucitala! :(\n";
@@ -54,10 +91,11 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
     GLFWwindow* window;
-    unsigned int wWidth = 1920;
-    unsigned int wHeight = 1080;
-    const char wTitle[] = "NLO";
+    unsigned int wWidth = SCR_WIDTH;
+    unsigned int wHeight = SCR_HEIGHT;
+    const char wTitle[] = "3D NLO";
     window = glfwCreateWindow(wWidth, wHeight, wTitle, glfwGetPrimaryMonitor(), NULL);
     if (window == NULL)
     {
@@ -65,9 +103,11 @@ int main()
         glfwTerminate();
         return 2;
     }
-
     glfwMakeContextCurrent(window);
-    glViewport(0, 0, wWidth, wHeight);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
     if (glewInit() != GLEW_OK)
     {
         cout << "GLEW nije mogao da se ucita! :'(\n";
@@ -137,40 +177,253 @@ int main()
     FT_Done_Face(face);
     FT_Done_FreeType(ft);
 
-    glClearColor(31 / 255.0f, 40 / 255.0f, 47 / 255.0f, 1.0f);
+
+    float skyboxVertices[] = {
+        // Positions          
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f
+    };
 
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
+    GLuint skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glBindVertexArray(0);
 
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
 
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    // -------------------------
+    Shader ourShader("shaders/model.vert", "shaders/model.frag");
+	Shader skyboxShader("shaders/skybox.vert", "shaders/skybox.frag");
+    // load models
+    // -----------
+    //Model ourModel("models/various-planets/planets.blend");
+    std::vector<Planet> planets = {
+        { Model("models/various-planets/p1/p1.obj"),   glm::vec3(0.0f, 0.0f, 2.0f),  glm::vec3(0.0f, 0.0f, 1.0f),  0.5f, glm::vec3(1.0f) },
+        { Model("models/various-planets/p2/p2.obj"),   glm::vec3(4.4f, 0.0f, 2.0f),  glm::vec3(0.7f, 1.0f, 0.0f),  1.0f, glm::vec3(1.0f) },
+        { Model("models/various-planets/p3/Planets.obj"), glm::vec3(0.4f, 4.5f, 0.0f), glm::vec3(0.3f, 0.5f, 0.3f),  0.33f, glm::vec3(3.5f) },
+        { Model("models/various-planets/p4/Planets.obj"), glm::vec3(6.4f, 8.5f, 0.0f), glm::vec3(0.8f, 0.2f, 0.1f),  0.5f, glm::vec3(1.5f) },
+        { Model("models/various-planets/p5/Planets.obj"), glm::vec3(8.4f, 3.5f, -2.0f), glm::vec3(1.8f, 0.2f, 0.1f), 0.5f, glm::vec3(1.5f) },
+        { Model("models/various-planets/p6/Planets.obj"), glm::vec3(-8.4f, 1.5f, -12.0f), glm::vec3(1.8f, 0.2f, 0.1f), 0.5f, glm::vec3(1.5f) },
+        { Model("models/various-planets/p7/Planets.obj"), glm::vec3(-8.4f, 1.5f, 12.0f), glm::vec3(1.8f, 0.2f, 0.1f), 0.5f, glm::vec3(1.5f) },
+        { Model("models/various-planets/p8/Planets.obj"), glm::vec3(3.4f, 6.5f, 9.0f), glm::vec3(1.8f, 0.2f, 0.1f), 0.5f, glm::vec3(1.5f) },
+        { Model("models/various-planets/p9/Planets.obj"), glm::vec3(1.4f, -3.5f, 9.0f), glm::vec3(1.8f, 0.2f, 0.1f), 0.5f, glm::vec3(1.5f) },
+        { Model("models/various-planets/p10/Planets.obj"), glm::vec3(-3.4f, 2.5f, 6.0f), glm::vec3(1.8f, 0.2f, 0.1f), 0.5f, glm::vec3(1.5f) },
+        { Model("models/various-planets/p11/Planets.obj"), glm::vec3(-4.7f, 10.5f, 8.0f), glm::vec3(1.8f, 0.2f, 0.1f), 0.5f, glm::vec3(1.5f) }
+    };
+
+    
+    std::vector<std::string> faces = {
+        "textures/space_right.jpg",
+        "textures/space_left.jpg",
+        "textures/space_top.jpg",
+        "textures/space_bottom.jpg",
+        "textures/space_front.jpg",
+        "textures/space_back.jpg"
+    };
+    GLuint cubemapTexture = loadCubemap(faces);
+    skyboxShader.use();
+    glUniform1i(glGetUniformLocation(skyboxShader.ID, "skybox"), 0);
+
+    // draw in wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++ RENDER LOOP - PETLJA ZA CRTANJE +++++++++++++++++++++++++++++++++++++++++++++++++
+
+    while (!glfwWindowShouldClose(window)) {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // don't forget to enable shader before setting uniforms
+        ourShader.use();
+
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
 
 
+        glDepthFunc(GL_LEQUAL);
+        skyboxShader.use();
+        glm::mat4 viewSky = glm::mat4(glm::mat3(camera.GetViewMatrix()));
+        skyboxShader.setMat4("view", viewSky);
+        skyboxShader.setMat4("projection", projection);
+        glBindVertexArray(skyboxVAO);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS);
 
-		glfwSwapBuffers(window);
-        capFPS();
+
+        // render the loaded model
+        for (Planet& planet : planets) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, planet.position);
+            model = glm::rotate(model, (float)glfwGetTime() * planet.rotationSpeed, planet.rotationAxis);
+            model = glm::scale(model, planet.scale);
+            ourShader.setMat4("model", model);
+            planet.model.Draw(ourShader);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+		capFPS();
     }
+
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++ POSPREMANJE +++++++++++++++++++++++++++++++++++++++++++++++++
+    glDeleteVertexArrays(1, &skyboxVAO);
+    glDeleteBuffers(1, &skyboxVBO);
 
     glfwTerminate();
     return 0;
 }
 
-void capFPS()
+unsigned int compileShader(GLenum type, const char* source)
 {
-    auto currentTime = std::chrono::high_resolution_clock::now();
-    double elapsedTime = std::chrono::duration<double>(currentTime - lastFrameTime).count();
-
-    if (elapsedTime < frameDuration)
+    //Uzima kod u fajlu na putanji "source", kompajlira ga i vraca sejder tipa "type"
+    //Citanje izvornog koda iz fajla
+    string content = "";
+    ifstream file(source);
+    stringstream ss;
+    if (file.is_open())
     {
-        std::this_thread::sleep_for(std::chrono::duration<double>(frameDuration - elapsedTime));
+        ss << file.rdbuf();
+        file.close();
+        cout << "Uspjesno procitao fajl sa putanje \"" << source << "\"!" << endl;
     }
-    lastFrameTime = std::chrono::high_resolution_clock::now();
+    else {
+        ss << "";
+        cout << "Greska pri citanju fajla sa putanje \"" << source << "\"!" << endl;
+    }
+    string temp = ss.str();
+    const char* sourceCode = temp.c_str(); //Izvorni kod sejdera koji citamo iz fajla na putanji "source"
+
+    int shader = glCreateShader(type); //Napravimo prazan sejder odredjenog tipa (vertex ili fragment)
+
+    int success; //Da li je kompajliranje bilo uspjesno (1 - da)
+    char infoLog[512]; //Poruka o gresci (Objasnjava sta je puklo unutar sejdera)
+    glShaderSource(shader, 1, &sourceCode, NULL); //Postavi izvorni kod sejdera
+    glCompileShader(shader); //Kompajliraj sejder
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success); //Provjeri da li je sejder uspjesno kompajliran
+    if (success == GL_FALSE)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog); //Pribavi poruku o gresci
+        if (type == GL_VERTEX_SHADER)
+            printf("VERTEX");
+        else if (type == GL_FRAGMENT_SHADER)
+            printf("FRAGMENT");
+        printf(" sejder ima gresku! Greska: \n");
+        printf(infoLog);
+    }
+    return shader;
 }
+
+unsigned int createShader(const char* vsSource, const char* fsSource)
+{
+    //Pravi objedinjeni sejder program koji se sastoji od Vertex sejdera ciji je kod na putanji vsSource
+
+    unsigned int program; //Objedinjeni sejder
+    unsigned int vertexShader; //Verteks sejder (za prostorne podatke)
+    unsigned int fragmentShader; //Fragment sejder (za boje, teksture itd)
+
+    program = glCreateProgram(); //Napravi prazan objedinjeni sejder program
+
+    vertexShader = compileShader(GL_VERTEX_SHADER, vsSource); //Napravi i kompajliraj vertex sejder
+    fragmentShader = compileShader(GL_FRAGMENT_SHADER, fsSource); //Napravi i kompajliraj fragment sejder
+
+    //Zakaci verteks i fragment sejdere za objedinjeni program
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program); //Povezi ih u jedan objedinjeni sejder program
+    glValidateProgram(program); //Izvrsi provjeru novopecenog programa
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &success); //Slicno kao za sejdere
+    if (success == GL_FALSE)
+    {
+        glGetShaderInfoLog(program, 512, NULL, infoLog);
+        cout << "Objedinjeni sejder ima gresku! Greska: \n";
+        cout << infoLog << endl;
+    }
+
+    //Posto su kodovi sejdera u objedinjenom sejderu, oni pojedinacni programi nam ne trebaju, pa ih brisemo zarad ustede na memoriji
+    glDetachShader(program, vertexShader);
+    glDeleteShader(vertexShader);
+    glDetachShader(program, fragmentShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+
 static unsigned loadImageToTexture(const char* filePath) {
     int TextureWidth;
     int TextureHeight;
@@ -202,13 +455,13 @@ static unsigned loadImageToTexture(const char* filePath) {
     }
     else
     {
-        std::cout << "Textura nije ucitana! Putanja texture: " << filePath << std::endl;
+        cout << "Textura nije ucitana! Putanja texture: " << filePath << endl;
         stbi_image_free(ImageData);
         return 0;
     }
 }
-void renderText(unsigned int shader, string text, float x, float y, float scale, glm::vec3 color, map<char, Character>& Characters, unsigned int VAO, unsigned int VBO)
-{
+
+void renderText(unsigned int shader, string text, float x, float y, float scale, glm::vec3 color, map<char, Character>& Characters, unsigned int VAO, unsigned int VBO) {
     // activate corresponding render state	
     glUseProgram(shader);
     glActiveTexture(GL_TEXTURE10);
@@ -217,7 +470,7 @@ void renderText(unsigned int shader, string text, float x, float y, float scale,
     glBindVertexArray(VAO);
 
     // iterate through all characters
-    std::string::const_iterator c;
+    string::const_iterator c;
     for (c = text.begin(); c != text.end(); c++)
     {
         Character ch = Characters[*c];
@@ -252,3 +505,98 @@ void renderText(unsigned int shader, string text, float x, float y, float scale,
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
+
+void capFPS()
+{
+    auto currentTime = chrono::high_resolution_clock::now();
+    double elapsedTime = chrono::duration<double>(currentTime - lastFrameTime).count();
+
+    if (elapsedTime < frameDuration)
+    {
+        this_thread::sleep_for(chrono::duration<double>(frameDuration - elapsedTime));
+    }
+    lastFrameTime = chrono::high_resolution_clock::now();
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+GLuint loadCubemap(std::vector<std::string> faces) {
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (GLuint i = 0; i < faces.size(); i++) {
+        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data) {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
